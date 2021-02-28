7345bacb3c05b28642c24334b17959b5a31f485e
Craig-Spencer-12 <craigspencer.linux@gmail.com>
2021-02-27 20:07:45 -0700

    feat: added cypress test runner to github actions

--- cypress/integration/quick-access.spec.js
+++ cypress/integration/quick-access.spec.js
@@ -8,36 +8,13 @@ const urlFixtures = require('../fixtures/urls.json')
 describe('Quick Access', () => {
   it('prepares to create the default cira config', () => {
 
-    //Ensure user is logged out
     cy.window().then((win) => {
-      win.sessionStorage.clear()
-    })
+      win.sessionStorage.clear();
+    });
 
-    //Go to web page
-    cy.visit(urlFixtures.base)
-
-    //Login
-    cy.get('.login-input')
-      .get('[id=userName]')
-      .type(loginFixtures.default.username)
-      .should('have.value', loginFixtures.default.username)
-    cy.get('.login-input')
-      .get('[id=password]')
-      .type(loginFixtures.default.password)
-      .should('have.value', loginFixtures.default.password)
-    cy.get('.login-btn')
-      .contains('Sign In')
-      .click()
-
-    //Enter RPS
-    cy.get('.rps-button')
-      .click()
-    cy.url().should('include', urlFixtures.page.rps)
-
-    //Navigate to CIRA config menu
-    cy.get('.nav-item')
-      .contains('CIRA Configs')
-      .click()
+    cy.visit(urlFixtures.base);
+    cy.login(loginFixtures.default.username, loginFixtures.default.password);
+    cy.get(".mat-list-item").contains("CIRA Configs").click();
 
     cy.pause()
   })
