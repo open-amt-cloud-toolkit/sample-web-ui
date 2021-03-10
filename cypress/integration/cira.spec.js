fef5e87890d62b7eaf6f644855f6dadd0def3dd5
Craig-Spencer-12 <craigspencer.linux@gmail.com>
2021-03-10 09:51:46 -0700

    refactor: deleted old cira test and renamed new cira test

--- cypress/integration/cira.spec.js
+++ cypress/integration/cira.spec.js
@@ -45,6 +45,11 @@ describe("Test CIRA Config Page", () => {
     it("creates the default CIRA config", () => {
       //Stub the get and post requests
       if (stubIt) {
+        cy.intercept("POST", /admin$/, {
+          statusCode: 200,
+          body: messageFixtures.MpsCertificate,
+        }).as("certificate");
+
         cy.intercept("POST", "ciraconfigs", {
           statusCode: 201,
           fixture: "stubResponses/Cira/post-success.json",
@@ -55,29 +60,181 @@ describe("Test CIRA Config Page", () => {
           fixture: "stubResponses/Cira/one-default.json",
         }).as("get-configs");
       } else {
-        cy.intercept("POST", "create").as("post-config");
+        cy.intercept("POST", /admin$/).as("certificate");
+        cy.intercept("POST", "ciraconfigs").as("post-config");
         cy.intercept("GET", "ciraconfigs").as("get-configs");
       }
 
-      cy.get("button").contains("Add New").click();
-
       //Fill out the config
-      cy.get("input").get("[name=configName]").type(ciraFixtures.default.name);
-      cy.get("input").get("[name=mpsServerAddress]").type(systemFixtures.ip);
-      cy.get("input")
-        .get("[name=username]")
-        .type(loginFixtures.default.username);
-      cy.get("input")
-        .get("[name=password]")
-        .type(loginFixtures.default.password);
-      cy.get("button[type=submit]").focus().type("{enter}");
+      cy.get("button").contains("Add New").click();
+      cy.enterCiraInfo(
+        ciraFixtures.default.name,
+        systemFixtures.ip,
+        loginFixtures.default.username,
+        loginFixtures.default.password
+      );
+      cy.get("button[type=submit]").click();
 
-      //Leaving out the assertion section as there are some errors
-      //running the sample-web-ui from a container and thus with github actions
+      //Wait for requests to finish and check them their responses
+      cy.wait("@post-config").then((req) => {
+        cy.wrap(req).its("response.statusCode").should("eq", 201);
+      });
+
+      //TODO: check the response to make sure that it is correct
+      //this is currently difficult because of the format of the response
+      cy.wait("@get-configs").its("response.statusCode").should("eq", 200);
+
+      // //Check that the config was successful
+      cy.get("mat-cell").contains(ciraFixtures.default.name);
+      cy.get("mat-cell").contains(systemFixtures.ip);
+      cy.get("mat-cell").contains(loginFixtures.default.username);
     });
 
-    //A run for testing the deletion exists but does not yet work with github actions
+    //The delete test relies on the create test above.
+    //This is bad style but these tests seems distict enough to
+    //warrant haveing seperate tests. Consider fixing this style
+    //issue if the tests become finicky or hard to understand
+    it("deletes the default cira config", () => {
+      //Stub the requests
+      if (stubIt) {
+        cy.intercept("DELETE", "ciraconfigs", {
+          statusCode: 204,
+        }).as("delete-config");
+
+        cy.intercept("GET", "ciraconfigs", {
+          statusCode: 200,
+          body: messageFixtures.empty,
+        }).as("get-configs");
+      } else {
+        cy.intercept("DELETE", "ciraconfigs").as("delete-config");
+        cy.intercept("GET", "ciraconfigs").as("get-configs");
+      }
+
+      //Delete CIRA Config (but cancel)
+      cy.get("mat-cell").contains("delete").click();
+      cy.get("button").contains("No").click();
+
+      //Check that the config was not deleted
+      cy.get("mat-cell").contains(ciraFixtures.default.name);
+      cy.get("mat-cell").contains(systemFixtures.ip);
+      cy.get("mat-cell").contains(loginFixtures.default.username);
+
+      //Delete CIRA Config
+      cy.get("mat-cell").contains("delete").click();
+      cy.get("button").contains("Yes").click();
+      cy.wait("@delete-config");
+
+      //Check that the config was deleted properly
+      cy.contains(ciraFixtures.default.name).should("not.exist");
+    });
   });
 
-  //More tests for the configs page are in the works but are not ready yet
+  context("attempt to create an invalid config", () => {
+    beforeEach("fills out the config", () => {
+      cy.get("button").contains("Add New").click();
+    });
+
+    afterEach("cancel the config", () => {
+      cy.get("button").contains("CANCEL").click({ timeout: 50000 });
+    });
+
+    it("enters an invalid config name", () => {
+      if (stubIt) {
+        cy.intercept("POST", /admin$/, {
+          statusCode: 200,
+          body: messageFixtures.MpsCertificate,
+        }).as("certificate");
+
+        cy.intercept("POST", "ciraconfigs", {
+          statusCode: 400,
+          fixture: "stubResponses/Cira/post-invalid-name.json",
+        }).as("post-config");
+
+        cy.intercept("GET", "ciraconfigs", {
+          statusCode: 200,
+          body: "[]",
+        }).as("get-configs");
+      } else {
+        cy.intercept("POST", /admin$/).as("certificate");
+        cy.intercept("POST", "ciraconfigs").as("post-config");
+        cy.intercept("GET", "ciraconfigs").as("get-configs");
+      }
+
+      cy.enterCiraInfo(
+        ciraFixtures.wrong.name,
+        systemFixtures.ip,
+        loginFixtures.default.username,
+        loginFixtures.default.password
+      );
+      cy.get("button[type=submit]").click({ timeout: 50000 });
+
+      cy.wait("@post-config").its("response.statusCode").should("eq", 400);
+    });
+
+    it("enters an invalid username", () => {
+      if (stubIt) {
+        cy.intercept("POST", /admin$/, {
+          statusCode: 200,
+          body: messageFixtures.MpsCertificate,
+        }).as("certificate");
+
+        cy.intercept("POST", "ciraconfigs", {
+          statusCode: 400,
+          fixture: "stubResponses/Cira/post-invalid-name.json",
+        }).as("post-config");
+
+        cy.intercept("GET", "ciraconfigs", {
+          statusCode: 200,
+          body: "[]",
+        }).as("get-configs");
+      } else {
+        cy.intercept("POST", /admin$/).as("certificate");
+        cy.intercept("POST", "ciraconfigs").as("post-config");
+        cy.intercept("GET", "ciraconfigs").as("get-configs");
+      }
+
+      cy.enterCiraInfo(
+        ciraFixtures.default.name,
+        systemFixtures.ip,
+        ciraFixtures.wrong.username,
+        loginFixtures.default.password
+      );
+      cy.get("button[type=submit]").click();
+
+      cy.wait("@post-config").its("response.statusCode").should("eq", 400);
+    });
+
+    it("enters an invalid password", () => {
+      if (stubIt) {
+        cy.intercept("POST", /admin$/, {
+          statusCode: 200,
+          body: messageFixtures.MpsCertificate,
+        }).as("certificate");
+
+        cy.intercept("POST", "ciraconfigs", {
+          statusCode: 400,
+          fixture: "stubResponses/Cira/post-invalid-name.json",
+        }).as("post-config");
+
+        cy.intercept("GET", "ciraconfigs", {
+          statusCode: 200,
+          body: "[]",
+        }).as("get-configs");
+      } else {
+        cy.intercept("POST", /admin$/).as("certificate");
+        cy.intercept("POST", "ciraconfigs").as("post-config");
+        cy.intercept("GET", "ciraconfigs").as("get-configs");
+      }
+
+      cy.enterCiraInfo(
+        ciraFixtures.default.name,
+        systemFixtures.ip,
+        loginFixtures.default.username,
+        ciraFixtures.wrong.password
+      );
+      cy.get("button[type=submit]").click();
+
+      cy.wait("@post-config").its("response.statusCode").should("eq", 400);
+    });
+  });
 });
