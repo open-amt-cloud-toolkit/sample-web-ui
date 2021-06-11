c66041f17467a2d41b3dadf199c24ac70eaaaaca
Mahendra-intel <77014068+Mahendra-intel@users.noreply.github.com>
2021-06-12 04:36:13 +0530

    feat(cira): update-cira-password (#256)
    * feat(cira): update-cira-password
    
    * fix(cira): resolved linting
    
    Co-authored-by: Mike <mjohanson@gesdate.com>
    Co-authored-by: Mike <michael.johanson@intel.com>

--- src/app/configs/config-detail/config-detail.component.ts
+++ src/app/configs/config-detail/config-detail.component.ts
@@ -1,7 +1,7 @@
 /*********************************************************************
-* Copyright (c) Intel Corporation 2021
-* SPDX-License-Identifier: Apache-2.0
-**********************************************************************/
+ * Copyright (c) Intel Corporation 2021
+ * SPDX-License-Identifier: Apache-2.0
+ **********************************************************************/
 import { Component, OnInit } from '@angular/core'
 import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 import { MatSnackBar } from '@angular/material/snack-bar'
@@ -24,15 +24,25 @@ export class ConfigDetailComponent implements OnInit {
   public pageTitle = 'New CIRA Config'
   public isEdit = false
   public errorMessages: string[] = []
-  constructor (public snackBar: MatSnackBar, public fb: FormBuilder, private readonly activeRoute: ActivatedRoute,
-    public router: Router, public configsService: ConfigsService) {
+  constructor (
+    public snackBar: MatSnackBar,
+    public fb: FormBuilder,
+    private readonly activeRoute: ActivatedRoute,
+    public router: Router,
+    public configsService: ConfigsService
+  ) {
     this.configForm = fb.group({
       configName: [null, Validators.required],
       mpsServerAddress: ['', Validators.required],
       serverAddressFormat: ['3', Validators.required], // 3 = ip, 201 = FQDN? wtf?
       commonName: [null, Validators.required],
       mpsPort: [4433, Validators.required],
-      username: [null, Validators.required],
+      username: ['admin', Validators.required],
+      generateRandomPassword: [false, Validators.required],
+      passwordLength: [
+        { value: null, disabled: true },
+        [Validators.max(32), Validators.min(8)]
+      ],
       password: [null, Validators.required],
       autoLoad: [true, Validators.required],
       mpsRootCertificate: [null],
@@ -44,28 +54,45 @@ export class ConfigDetailComponent implements OnInit {
   // ^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|$)){4}$
 
   ngOnInit (): void {
-    this.activeRoute.params.subscribe(params => {
+    this.activeRoute.params.subscribe((params) => {
       if (params.name != null && params.name !== '') {
         this.isLoading = true
-        this.configsService.getRecord(params.name).pipe(
-          finalize(() => {
-            this.isLoading = false
-          })).subscribe(data => {
-          this.isEdit = true
-          this.pageTitle = data.configName
-          this.configForm.controls.configName.disable()
-          this.configForm.patchValue(data)
-          this.configForm.patchValue({ serverAddressFormat: data.serverAddressFormat.toString() })
-        },
-        error => {
-          this.errorMessages = error
-        })
+        this.configsService
+          .getRecord(params.name)
+          .pipe(
+            finalize(() => {
+              this.isLoading = false
+            })
+          )
+          .subscribe(
+            (data) => {
+              this.isEdit = true
+              this.pageTitle = data.configName
+              this.configForm.controls.configName.disable()
+              this.configForm.patchValue(data)
+              this.configForm.patchValue({
+                serverAddressFormat: data.serverAddressFormat.toString()
+              })
+            },
+            (error) => {
+              this.errorMessages = error
+            }
+          )
       }
     })
 
-    this.configForm.controls.serverAddressFormat?.valueChanges.subscribe(value => this.serverAddressFormatChange(+value))
-    this.configForm.controls.autoLoad?.valueChanges.subscribe(value => this.autoLoadChange(value))
-    this.configForm.controls.mpsServerAddress?.valueChanges.subscribe(value => this.serverAddressChange(value))
+    this.configForm.controls.serverAddressFormat?.valueChanges.subscribe(
+      (value) => this.serverAddressFormatChange(+value)
+    )
+    this.configForm.controls.autoLoad?.valueChanges.subscribe((value) =>
+      this.autoLoadChange(value)
+    )
+    this.configForm.controls.mpsServerAddress?.valueChanges.subscribe((value) =>
+      this.serverAddressChange(value)
+    )
+    this.configForm.controls.generateRandomPassword?.valueChanges.subscribe(
+      (value) => this.generateRandomPasswordChange(value)
+    )
   }
 
   serverAddressChange (value: string): void {
@@ -83,17 +110,38 @@ export class ConfigDetailComponent implements OnInit {
       this.configForm.controls.mpsRootCertificate?.disable()
       this.configForm.controls.mpsRootCertificate?.clearValidators()
     } else {
-      this.configForm.controls.mpsRootCertificate?.setValidators(Validators.required)
+      this.configForm.controls.mpsRootCertificate?.setValidators(
+        Validators.required
+      )
       this.configForm.controls.mpsRootCertificate?.enable()
     }
   }
 
   serverAddressFormatChange (value: number): void {
-    if (value === 3) { // ipv4
+    if (value === 3) {
+      // ipv4
       this.configForm.controls.commonName?.enable()
-    } else { // fqdn
+    } else {
+      // fqdn
       this.configForm.controls.commonName?.disable()
       this.configForm.controls.commonName?.setValue(null)
+    }
+  }
+
+  generateRandomPasswordChange (value: boolean): void {
+    if (value) {
+      this.configForm.controls.password.disable()
+      this.configForm.controls.password.setValue(null)
+      this.configForm.controls.password.clearValidators()
+      this.configForm.controls.password.setValue(null)
+      this.configForm.controls.passwordLength.setValidators([Validators.max(32), Validators.min(8)])
+      this.configForm.controls.passwordLength.enable()
+    } else {
+      this.configForm.controls.password.enable()
+      this.configForm.controls.password.setValidators(Validators.required)
+      this.configForm.controls.passwordLength.disable()
+      this.configForm.controls.passwordLength.setValue(null)
+      this.configForm.controls.passwordLength.clearValidators()
     }
   }
 
@@ -101,9 +149,11 @@ export class ConfigDetailComponent implements OnInit {
     return this.configForm.controls.autoLoad?.value === true
   }
 
-  trimRootCert = (cert: string): string => cert.replace('-----BEGIN CERTIFICATE-----', '')
-    .replace('-----END CERTIFICATE-----', '')
-    .replace(/\s/g, '')
+  trimRootCert = (cert: string): string =>
+    cert
+      .replace('-----BEGIN CERTIFICATE-----', '')
+      .replace('-----END CERTIFICATE-----', '')
+      .replace(/\s/g, '')
 
   onSubmit (): void {
     if (this.configForm.valid) {
@@ -125,32 +175,54 @@ export class ConfigDetailComponent implements OnInit {
       // todo: don't do it this way
       if (result.autoLoad) {
         delete result.autoLoad
-        this.configsService.loadMPSRootCert().pipe(
-          finalize(() => {
-            this.isLoading = false
-          }),
-          mergeMap(data => {
-            result.mpsRootCertificate = this.trimRootCert(data)
-            return rpsRequest
-          })).subscribe(data => {
-          this.snackBar.open($localize`CIRA ${reqType} created successfully`, undefined, SnackbarDefaults.defaultSuccess)
-          // eslint-disable-next-line @typescript-eslint/no-floating-promises
-          this.router.navigate(['/ciraconfigs'])
-        }, error => {
-          console.log('error', error)
-          this.errorMessages = error
-        })
+        this.configsService
+          .loadMPSRootCert()
+          .pipe(
+            finalize(() => {
+              this.isLoading = false
+            }),
+            mergeMap((data) => {
+              result.mpsRootCertificate = this.trimRootCert(data)
+              return rpsRequest
+            })
+          )
+          .subscribe(
+            (data) => {
+              this.snackBar.open(
+                $localize`CIRA ${reqType} created successfully`,
+                undefined,
+                SnackbarDefaults.defaultSuccess
+              )
+              // eslint-disable-next-line @typescript-eslint/no-floating-promises
+              this.router.navigate(['/ciraconfigs'])
+            },
+            (error) => {
+              console.log('error', error)
+              this.errorMessages = error
+            }
+          )
       } else {
         delete result.autoLoad
-        rpsRequest.pipe(finalize(() => {
-          this.isLoading = false
-        })).subscribe(data => {
-          this.snackBar.open($localize`CIRA ${reqType} created successfully`, undefined, SnackbarDefaults.defaultSuccess)
-          // eslint-disable-next-line @typescript-eslint/no-floating-promises
-          this.router.navigate(['/ciraconfigs'])
-        }, error => {
-          this.errorMessages = error
-        })
+        rpsRequest
+          .pipe(
+            finalize(() => {
+              this.isLoading = false
+            })
+          )
+          .subscribe(
+            (data) => {
+              this.snackBar.open(
+                $localize`CIRA ${reqType} created successfully`,
+                undefined,
+                SnackbarDefaults.defaultSuccess
+              )
+              // eslint-disable-next-line @typescript-eslint/no-floating-promises
+              this.router.navigate(['/ciraconfigs'])
+            },
+            (error) => {
+              this.errorMessages = error
+            }
+          )
       }
     }
   }
