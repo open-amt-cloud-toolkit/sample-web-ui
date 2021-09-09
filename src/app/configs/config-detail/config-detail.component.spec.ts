c849a7e4a7312aa8d63183211cd01c71630068aa
Craig-Spencer-12 <craigspencer.linux@gmail.com>
2021-09-08 17:36:29 -0700

    revert(profile): adds client side pass generation

--- src/app/configs/config-detail/config-detail.component.spec.ts
+++ src/app/configs/config-detail/config-detail.component.spec.ts
@@ -3,7 +3,7 @@
 * SPDX-License-Identifier: Apache-2.0
 **********************************************************************/
 import { NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'
-import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { ActivatedRoute } from '@angular/router'
 import { RouterTestingModule } from '@angular/router/testing'
@@ -17,10 +17,16 @@ describe('ConfigDetailComponent', () => {
   let component: ConfigDetailComponent
   let fixture: ComponentFixture<ConfigDetailComponent>
   let getRecordSpy: jasmine.Spy
+  let updateRecordSpy: jasmine.Spy
+  let loadMpsRootCertSpy: jasmine.Spy
+  let createRecordSpy: jasmine.Spy
 
   beforeEach(async () => {
-    const configsService = jasmine.createSpyObj('ConfigsService', ['getRecord'])
-    getRecordSpy = configsService.getRecord.and.returnValue(of({ serverAddressFormat: 3 }))
+    const configsService = jasmine.createSpyObj('ConfigsService', ['getRecord', 'update', 'loadMPSRootCert', 'create'])
+    getRecordSpy = configsService.getRecord.and.returnValue(of({ serverAddressFormat: 3, configName: 'ciraConfig1' }))
+    updateRecordSpy = configsService.update.and.returnValue(of({}))
+    loadMpsRootCertSpy = configsService.loadMPSRootCert.and.returnValue(of('root certificate'))
+    createRecordSpy = configsService.create.and.returnValue(of({}))
 
     await TestBed.configureTestingModule({
       imports: [BrowserAnimationsModule, SharedModule, RouterTestingModule.withRoutes([])],
@@ -46,5 +52,85 @@ describe('ConfigDetailComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy()
     expect(getRecordSpy.calls.any()).toBe(true, 'getRecord called')
+    expect(component.isEdit).toBeTrue()
+    expect(component.pageTitle).toEqual('ciraConfig1')
+  })
+
+  it('should cancel', async () => {
+    const routerSpy = spyOn(component.router, 'navigate')
+    await component.cancel()
+    expect(routerSpy).toHaveBeenCalledWith(['/ciraconfigs'])
+  })
+
+  it('should update the server address format on change', fakeAsync(() => {
+    component.configForm.get('serverAddressFormat')?.setValue('201')
+    component.configForm.get('serverAddressFormat')?.updateValueAndValidity({ emitEvent: true })
+    tick()
+    fixture.detectChanges()
+    expect(component.configForm.get('commonName')?.value).toEqual(null)
+  }))
+
+  it('should set the common name same as server address when format is ip address', fakeAsync(() => {
+    component.configForm.get('mpsServerAddress')?.setValue('255.255.255.1')
+    component.configForm.get('mpsServerAddress')?.updateValueAndValidity({ emitEvent: true })
+    tick()
+    fixture.detectChanges()
+    expect(component.configForm.get('commonName')?.value).toEqual('255.255.255.1')
+  }))
+
+  it('should enable the auto MPS root certification load control when autoload is set to false', fakeAsync(() => {
+    component.configForm.get('autoLoad')?.setValue(false)
+    component.configForm.get('autoLoad')?.updateValueAndValidity({ emitEvent: true })
+    tick()
+    fixture.detectChanges()
+    expect(component.configForm.get('mpsRootCertificate')?.enabled).toBeTrue()
+  }))
+
+  it('should submit when valid(update)', () => {
+    const routerSpy = spyOn(component.router, 'navigate')
+    component.configForm.patchValue({
+      configName: 'ciraConfig1',
+      mpsServerAddress: '255.255.255.255',
+      serverAddressFormat: '3', // 3 = ip, 201 = FQDN? wtf?
+      commonName: '255.255.255.255',
+      mpsPort: 4433,
+      username: 'admin',
+      autoLoad: true,
+      mpsRootCertificate: 'rootcert',
+      proxyDetails: null,
+      regeneratePassword: false
+    })
+    expect(component.configForm.valid).toBeTruthy()
+
+    component.onSubmit()
+
+    expect(component.isLoading).toBeFalse()
+    expect(loadMpsRootCertSpy).toHaveBeenCalled()
+    expect(updateRecordSpy).toHaveBeenCalled()
+    expect(routerSpy).toHaveBeenCalled()
+  })
+
+  it('should submit when valid(create)', () => {
+    const routerSpy = spyOn(component.router, 'navigate')
+    component.configForm.patchValue({
+      configName: 'ciraConfig2',
+      mpsServerAddress: '255.255.255.255',
+      serverAddressFormat: '3', // 3 = ip, 201 = FQDN? wtf?
+      commonName: '255.255.255.255',
+      mpsPort: 4433,
+      username: 'admin',
+      autoLoad: true,
+      mpsRootCertificate: 'rootcert',
+      proxyDetails: null,
+      regeneratePassword: false
+    })
+    component.isEdit = false
+    expect(component.configForm.valid).toBeTruthy()
+
+    component.onSubmit()
+
+    expect(component.isLoading).toBeFalse()
+    expect(createRecordSpy).toHaveBeenCalled()
+    expect(routerSpy).toHaveBeenCalled()
   })
 })
--- src/app/profiles/profiles.service.spec.ts
+++ src/app/profiles/profiles.service.spec.ts
@@ -4,16 +4,17 @@
 **********************************************************************/
 import { TestBed } from '@angular/core/testing'
 import { Router } from '@angular/router'
+import { of } from 'rxjs'
 import { AuthService } from '../auth.service'
 
 import { ProfilesService } from './profiles.service'
 
 describe('ProfilesService', () => {
   let service: ProfilesService
-  let httpClientSpy: { get: jasmine.Spy }
+  let httpClientSpy: { get: jasmine.Spy, post: jasmine.Spy, patch: jasmine.Spy, delete: jasmine.Spy }
   let routerSpy
   beforeEach(() => {
-    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get'])
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get', 'post', 'patch', 'delete'])
     routerSpy = jasmine.createSpyObj('Router', ['navigate'])
     TestBed.configureTestingModule({})
     service = new ProfilesService(new AuthService(httpClientSpy as any, routerSpy as Router), httpClientSpy as any)
@@ -21,5 +22,116 @@ describe('ProfilesService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy()
+  })
+
+  it('should load all the profiles when get all profile request fired', (done: DoneFn) => {
+    const profileResponse = {
+      data: [{
+        profileName: 'profile1',
+        amtPassword: null,
+        configurationScript: null,
+        activation: 'acmactivate',
+        ciraConfigName: 'config1',
+        dhcpEnabled: true,
+        mebxPassword: 'password',
+        generateRandomPassword: true,
+        generateRandomMEBxPassword: true,
+        tags: ['acm'],
+        wifiConfigs: []
+      }],
+      totalCount: 1
+    }
+
+    httpClientSpy.get.and.returnValue(of(profileResponse))
+
+    service.getData().subscribe(response => {
+      expect(response).toEqual(profileResponse)
+      done()
+    })
+
+    expect(httpClientSpy.get.calls.count()).toEqual(1)
+  })
+
+  it('should return a specific profile when a single profile is requested', (done: DoneFn) => {
+    const profileResponse = {
+      profileName: 'profile1',
+      amtPassword: null,
+      configurationScript: null,
+      activation: 'acmactivate',
+      ciraConfigName: 'config1',
+      dhcpEnabled: true,
+      mebxPassword: 'password',
+      generateRandomPassword: true,
+      generateRandomMEBxPassword: true,
+      tags: ['acm'],
+      wifiConfigs: []
+    }
+
+    httpClientSpy.get.and.returnValue(of(profileResponse))
+
+    service.getRecord('profile1').subscribe(response => {
+      expect(response).toEqual(profileResponse)
+      done()
+    })
+  })
+
+  it('should delete the specified profile when a delete request fires', (done: DoneFn) => {
+    httpClientSpy.delete.and.returnValue(of({}))
+
+    service.delete('profile1').subscribe(() => {
+      done()
+    })
+
+    expect(httpClientSpy.delete.calls.count()).toEqual(1)
+  })
+
+  it('should create the profile when post request gets fired', (done: DoneFn) => {
+    const profileReq = {
+      profileName: 'profile1',
+      amtPassword: null,
+      configurationScript: null,
+      activation: 'acmactivate',
+      ciraConfigName: 'config1',
+      dhcpEnabled: true,
+      mebxPassword: 'password',
+      generateRandomPassword: true,
+      generateRandomMEBxPassword: true,
+      tags: ['acm'],
+      wifiConfigs: []
+    }
+
+    httpClientSpy.post.and.returnValue(of(profileReq))
+
+    service.create(profileReq).subscribe(response => {
+      expect(response).toEqual(profileReq)
+      done()
+    })
+
+    expect(httpClientSpy.post.calls.count()).toEqual(1)
+  })
+
+  it('should update the profile when a patch request fired', (done: DoneFn) => {
+    const profileReq = {
+      profileName: 'profile1',
+      amtPassword: null,
+      configurationScript: null,
+      activation: 'acmactivate',
+      ciraConfigName: 'config1',
+      dhcpEnabled: true,
+      mebxPassword: 'password',
+      generateRandomPassword: true,
+      generateRandomMEBxPassword: true,
+      tags: ['acm'],
+      wifiConfigs: []
+    }
+
+    httpClientSpy.patch.and.returnValue(of(profileReq))
+
+    service.update(profileReq).subscribe(response => {
+      expect(response).toEqual(profileReq)
+      done()
+    })
+
+    expect(httpClientSpy.patch.calls.count()).toEqual(1)
   })
 })
