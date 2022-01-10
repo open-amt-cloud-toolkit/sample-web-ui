fba1633b6a218fe0d799280ed300e7a298c0c912
omammadx <orkhanx.mammadli@intel.com>
2022-01-10 12:15:45 -0500

    minor fix

--- cypress/integration/rpc/amtdevice.spec.ts
+++ cypress/integration/rpc/amtdevice.spec.ts
@@ -1,12 +1,9 @@
-
 describe('Successful execution of pre-provisioning on amtdevice', () => {
   it('Control Mode is pre-provisioning', () => {
-    cy.exec('docker run --device=/dev/mei0 intel/oact-rpc-go:latest amtinfo')
+    cy.exec('docker run --device=/dev/mei0 intel/oact-rpc-go:latest amtinfo', {failOnNonZeroExit: false})
     .its('stdout')
     .should('contain','pre-provisioning state');
   });
   
 
 });
-
-// docker run --device=/dev/mei0 vprodemo.azurecr.io/rpc:latest --amtinfo all
