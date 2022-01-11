0d107ed57041783fe609805ba865402a223cd84e
Mike <michael.johanson@intel.com>
2022-01-10 19:35:07 -0700

    increase exec timeout

--- cypress/integration/rpc/amtdevice.spec.ts
+++ cypress/integration/rpc/amtdevice.spec.ts
@@ -1,7 +1,8 @@
 describe('Successful execution of pre-provisioning on amtdevice', () => {
-  it('Control Mode is pre-provisioning', () => {
+  it('Control Mode is pre-provisioning', { execTimeout: 90000 }, () => {
     cy.exec('docker run --device=/dev/mei0 intel/oact-rpc-go:latest amtinfo', { failOnNonZeroExit: false }).then((result) => {
-      expect(result.stdout).to.contain('pre-provisioning state')
+      cy.log(result.stderr)
+      expect(result.stderr).to.contain('pre-provisioning state')
     })
   })
 })
