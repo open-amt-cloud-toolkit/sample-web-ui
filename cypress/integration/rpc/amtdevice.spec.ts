87bc799ad8638efd333292557b5c9135df4f371a
omammadx <orkhanx.mammadli@intel.com>
2022-01-19 17:39:07 -0500

    testing

--- cypress/integration/rpc/amtdevice.spec.ts
+++ cypress/integration/rpc/amtdevice.spec.ts
@@ -7,14 +7,14 @@ describe('Successful execution of pre-provisioning on amtdevice', () => {
   })
 
   it('Control Mode is pre-provisioning with Deacivate Device', { execTimeout: 90000 }, () => {
-    cy.exec('docker run --device=/dev/mei0 intel/oact-rpc-go:latest --url deactivate -u wss://cicdoact.eastus.azurecontainer.io:8443/activate --password ${{ secrets.AMT_PASSWORD }', { failOnNonZeroExit: false }).then((result) => {
+    cy.exec('docker run --device=/dev/mei0 intel/oact-rpc-go:latest --url deactivate -u wss://cicdoact.eastus.azurecontainer.io:8443/activate -v -n -json --password ${{ secrets.AMT_PASSWORD }}', { failOnNonZeroExit: false }).then((result) => {
       cy.log(result.stdout)
       expect(result.stdout).to.contain('pre-provisioning state')
     })
   })
 
   it('Control Mode is pre-provisioning with Activate Device', { execTimeout: 90000 }, () => {
-    cy.exec('docker run --device=/dev/mei0 intel/oact-rpc-go:latest activate -u wss://cicdoact.eastus.azurecontainer.io:8443/activate --profile ${profileName}',{ failOnNonZeroExit: false }).then((result) => {
+    cy.exec('docker run --device=/dev/mei0 intel/oact-rpc-go:latest activate -u wss://cicdoact.eastus.azurecontainer.io:8443/activate -v -n --profile happyPath',{ failOnNonZeroExit: false }).then((result) => {
       // cy.exec('docker run --device=/dev/mei0 intel/oact-rpc-go:latest version', { failOnNonZeroExit: false }).then((result) => {
       cy.log(result.stdout)
       console.log(result.stdout)
