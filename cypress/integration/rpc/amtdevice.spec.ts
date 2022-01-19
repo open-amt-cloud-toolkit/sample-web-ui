5bfb1740da5a6a75780829af92ed99ec6ffd58d2
omammadx <orkhanx.mammadli@intel.com>
2022-01-19 16:04:49 -0500

    testing

--- cypress/integration/rpc/amtdevice.spec.ts
+++ cypress/integration/rpc/amtdevice.spec.ts
@@ -7,7 +7,7 @@ describe('Successful execution of pre-provisioning on amtdevice', () => {
   })
 
   it('Control Mode is pre-provisioning with Activate Device', { execTimeout: 90000 }, () => {
-    cy.exec('docker run --device=/dev/mei0 intel/oact-rpc-go:latest activate -u wss://cicdoact.eastus.azurecontainer.io:8443/activate -v -n --profile happyPath',{ failOnNonZeroExit: false }).then((result) => {
+    cy.exec('docker run --device=/dev/mei0 intel/oact-rpc-go:latest activate -u wss://cicdoact.eastus.azurecontainer.io:8443/activate -v -n --profile happyPath --password P@ssw0rd',{ failOnNonZeroExit: false }).then((result) => {
       // cy.exec('docker run --device=/dev/mei0 intel/oact-rpc-go:latest version', { failOnNonZeroExit: false }).then((result) => {
       cy.log(result.stdout)
       console.log(result.stdout)
