75307cf0be7c8cdf404fce598509e3c4c6bb81fa
omammadx <orkhanx.mammadli@intel.com>
2022-01-19 17:49:07 -0500

    testing

--- cypress/integration/rpc/amtdevice.spec.ts
+++ cypress/integration/rpc/amtdevice.spec.ts
@@ -7,7 +7,7 @@ describe('Successful execution of pre-provisioning on amtdevice', () => {
   })
 
   it('Control Mode is pre-provisioning with Deacivate Device', { execTimeout: 90000 }, () => {
-    cy.exec('docker run --device=/dev/mei0 intel/oact-rpc-go:latest --url deactivate -u wss://cicdoact.eastus.azurecontainer.io:8443/activate -v -n -json --password ${{ secrets.AMT_PASSWORD }}', { failOnNonZeroExit: false }).then((result) => {
+    cy.exec('docker run --device=/dev/mei0 intel/oact-rpc-go:latest deactivate -u wss://cicdoact.eastus.azurecontainer.io:8443/activate -v -n -json --password ${{ secrets.AMT_PASSWORD }}', { failOnNonZeroExit: false }).then((result) => {
       cy.log(result.stdout)
       expect(result.stdout).to.contain('pre-provisioning state')
     })
