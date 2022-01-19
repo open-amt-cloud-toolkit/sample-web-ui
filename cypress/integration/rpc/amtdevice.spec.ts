fb6d23b68c081d9f24bb36c102cf83a6453f047b
omammadx <orkhanx.mammadli@intel.com>
2022-01-19 18:13:27 -0500

    testing

--- cypress/integration/rpc/amtdevice.spec.ts
+++ cypress/integration/rpc/amtdevice.spec.ts
@@ -1,6 +1,5 @@
 describe('Successful execution of pre-provisioning on amtdevice', () => {
   it('Control Mode is pre-provisioning with AMT info ALL', () => {
-    cy.wait(90000)
     cy.exec('docker run --device=/dev/mei0 intel/oact-rpc-go:latest amtinfo', { failOnNonZeroExit: false }).then((result) => {
       cy.log(result.stderr)
       expect(result.stderr).to.contain('pre-provisioning state')
