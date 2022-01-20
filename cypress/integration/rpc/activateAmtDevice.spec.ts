967edcbbb90ec4156e965d4c1c3ed86d0549b39c
Mike <michael.johanson@intel.com>
2022-01-20 14:29:48 -0700

    add log for stderr

--- cypress/integration/rpc/activateAmtDevice.spec.ts
+++ cypress/integration/rpc/activateAmtDevice.spec.ts
@@ -1,6 +1,6 @@
 describe('Deacivating AMT device', () => {
   it('Deacivate AMT device', { execTimeout: 90000 }, () => {
-    cy.exec('docker run --device=/dev/mei0 intel/oact-rpc-go:latest activate -u wss://cicdoact.eastus.azurecontainer.io:8443/activate -v -n --profile happyPath',{ failOnNonZeroExit: false }).then((result) => {
+    cy.exec('docker run --device=/dev/mei0 intel/oact-rpc-go:latest activate -u wss://cicdoact.eastus.azurecontainer.io:8443/activate -v -n --profile happyPath', { failOnNonZeroExit: false }).then((result) => {
       // cy.exec('docker run --device=/dev/mei0 intel/oact-rpc-go:latest version', { failOnNonZeroExit: false }).then((result) => {
       cy.log(result.stdout)
       console.log(result.stdout)
--- cypress/integration/rpc/deactivateAmtDevice.spec.ts
+++ cypress/integration/rpc/deactivateAmtDevice.spec.ts
@@ -2,6 +2,7 @@ describe('Deactivating AMT device', () => {
   it('Deacivate AMT device', { execTimeout: 90000 }, () => {
     cy.exec('docker run --device=/dev/mei0 intel/oact-rpc-go:latest deactivate -u wss://cicdoact.eastus.azurecontainer.io:8443/activate -v -n -json --password P@ssw0rd', { failOnNonZeroExit: false }).then((result) => {
       cy.log(result.stdout)
+      cy.log(result.stderr)
       expect(result.stdout).to.contain('pre-provisioning state')
     })
   })
