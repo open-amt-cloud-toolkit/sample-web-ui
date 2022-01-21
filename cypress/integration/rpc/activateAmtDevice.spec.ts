b6086f3cf5083ae27b32fec18b2644b7f3465130
Mike <michael.johanson@intel.com>
2022-01-21 09:28:51 -0700

    increase timeout

--- cypress/integration/rpc/activateAmtDevice.spec.ts
+++ cypress/integration/rpc/activateAmtDevice.spec.ts
@@ -1,5 +1,5 @@
 describe('Activating AMT device', () => {
-  it('Activating AMT device', { execTimeout: 90000 }, () => {
+  it('Activating AMT device', { execTimeout: 120000 }, () => {
     cy.exec('docker run --device=/dev/mei0 intel/oact-rpc-go:latest activate -u wss://cicdoact.eastus.azurecontainer.io:8443/activate -v -n --profile happyPath', { failOnNonZeroExit: false }).then((result) => {
       // cy.exec('docker run --device=/dev/mei0 intel/oact-rpc-go:latest version', { failOnNonZeroExit: false }).then((result) => {
       cy.log(result.stdout)
