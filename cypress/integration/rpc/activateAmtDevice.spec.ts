cd4f3ba13067b1462ed3bb303351cc8faa738563
Mike <michael.johanson@intel.com>
2022-01-21 10:07:16 -0700

    update expectations

--- cypress/integration/rpc/activateAmtDevice.spec.ts
+++ cypress/integration/rpc/activateAmtDevice.spec.ts
@@ -1,10 +1,9 @@
 describe('Activating AMT device', () => {
   it('Activating AMT device', { execTimeout: 240000 }, () => {
-    cy.exec('docker run --device=/dev/mei0 intel/oact-rpc-go:latest activate -u wss://cicdoact.eastus.azurecontainer.io:8443/activate -v -n --profile happyPath', { failOnNonZeroExit: false }).then((result) => {
-      // cy.exec('docker run --device=/dev/mei0 intel/oact-rpc-go:latest version', { failOnNonZeroExit: false }).then((result) => {
-      cy.log(result.stdout)
-      cy.log(result.stderr)
-      expect(result.stdout).to.contain('activated')
+    cy.exec('docker run --device=/dev/mei0 intel/oact-rpc-go:latest activate -u wss://cicdoact.eastus.azurecontainer.io:8443/activate -v -n --profile happyPath', { failOnNonZeroExit: false }).then((result) => {      
+      expect(result.stderr).to.contain('Status: Client control mode')
+      expect(result.stderr).to.contain('Network: Ethernet Configured.')
+      expect(result.stderr).to.contain('CIRA: Configured')
     })
   })
 })
