daf3f059b1dc35a5b4949484fd445b1f707a2ddb
omammadx <orkhanx.mammadli@intel.com>
2022-01-25 16:55:47 -0500

    changing value

--- cypress/integration/rpc/activateAmtDevice.spec.ts
+++ cypress/integration/rpc/activateAmtDevice.spec.ts
@@ -1,6 +1,6 @@
 describe('Activating AMT device', () => {
   it('Activating AMT device', { execTimeout: 240000 }, () => {
-    cy.exec('docker run --device=/dev/mei0 intel/oact-rpc-go:latest activate -u wss://cicdoact.eastus.azurecontainer.io:8443/activate -v -n --profile ${profileName}', { failOnNonZeroExit: false }).then((result) => {      
+    cy.exec('docker run --device=/dev/mei0 intel/oact-rpc-go:latest activate -u ${profileName}/activate -v -n --profile ${profileName}', { failOnNonZeroExit: false }).then((result) => {      
       expect(result.stderr).to.contain('Status: Client control mode')
       expect(result.stderr).to.contain('Network: Ethernet Configured.')
       expect(result.stderr).to.contain('CIRA: Configured')
