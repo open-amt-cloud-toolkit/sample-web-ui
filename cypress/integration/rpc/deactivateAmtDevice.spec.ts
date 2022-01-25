af43a86efefe14a8c93945f2d8134f5fe6a6d430
omammadx <orkhanx.mammadli@intel.com>
2022-01-25 12:24:41 -0500

    changing value

--- cypress/integration/rpc/deactivateAmtDevice.spec.ts
+++ cypress/integration/rpc/deactivateAmtDevice.spec.ts
@@ -1,6 +1,6 @@
 describe('Deactivating AMT device', () => {
   it('Deacivate AMT device', { execTimeout: 90000 }, () => {
-    cy.exec('docker run --device=/dev/mei0 vprodemo.azurecr.io/rpc-go:latest deactivate -u wss://cicdoact.eastus.azurecontainer.io:8443/activate -v -n -f -json --password ${{AMT_PASSWORD}}', { failOnNonZeroExit: false }).then((result) => {
+    cy.exec('docker run --device=/dev/mei0 vprodemo.azurecr.io/rpc-go:latest deactivate -u wss://cicdoact.eastus.azurecontainer.io:8443/activate -v -n -f -json --password ${CYPRESS_AMTPASSWORD}', { failOnNonZeroExit: false }).then((result) => {
        expect(result.stderr).to.contain('Status: Deactivated')
     })
   })
