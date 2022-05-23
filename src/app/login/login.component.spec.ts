1f0bc2cd69c2729ffe80e24948ff210d6130a007
madhavilosetty-intel <madhavi.losetty@intel.com>
2022-05-23 09:26:29 -0700

    feat(login): MPS web_auth_enabled set to false, sample web ui cannot be accessed.
    AB#7073

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -6,6 +6,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing'
 import { ReactiveFormsModule } from '@angular/forms'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { Router } from '@angular/router'
+import { of, throwError } from 'rxjs'
 import { AuthService } from '../auth.service'
 import { SharedModule } from '../shared/shared.module'
 
@@ -15,11 +16,12 @@ describe('LoginComponent', () => {
   let component: LoginComponent
   let fixture: ComponentFixture<LoginComponent>
   let routerSpy
-  let authService
+  let authService: any
+  let loginSpy: jasmine.Spy
+  let loginErrorSpy: jasmine.Spy
   beforeEach(async () => {
     routerSpy = jasmine.createSpyObj('Router', ['navigate'])
-    authService = jasmine.createSpyObj('AuthService', ['isLoggedIn'])
-
+    authService = jasmine.createSpyObj('AuthService', ['login'])
     await TestBed.configureTestingModule({
       imports: [BrowserAnimationsModule, SharedModule, ReactiveFormsModule],
       declarations: [LoginComponent],
@@ -35,5 +37,29 @@ describe('LoginComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy()
+  })
+  it('Should call login service when onSubmit()', async () => {
+    loginSpy = authService.login.and.returnValue(of({ token: 'abc' }))
+    Object.defineProperty(component.loginForm, 'valid', {
+      get: () => true
+    })
+    await component.onSubmit()
+    expect(loginSpy).toHaveBeenCalled()
+  })
+  it('Should fail when login service throws 405 status', async () => {
+    loginErrorSpy = authService.login.and.returnValue(throwError({ status: 405, error: { message: 'failed' } }))
+    Object.defineProperty(component.loginForm, 'valid', {
+      get: () => true
+    })
+    await component.onSubmit()
+    expect(loginErrorSpy).toHaveBeenCalled()
+  })
+  it('Should fail when login service throws an error', async () => {
+    loginErrorSpy = authService.login.and.returnValue(throwError({ status: 401, error: { message: 'failed' } }))
+    Object.defineProperty(component.loginForm, 'valid', {
+      get: () => true
+    })
+    await component.onSubmit()
+    expect(loginErrorSpy).toHaveBeenCalled()
   })
 })
