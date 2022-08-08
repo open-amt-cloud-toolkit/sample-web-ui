open-amt-cloud-toolkit/sample-web-ui

commit 8d0280c2403e439f2f4e0ac607f13bc6878d6f45

build(deps): upgrade to angular14 (#769)
Co-authored-by: madhavilosetty-intel <madhavi.losetty@intel.com>

Dockerfile
@@ -8,7 +8,7 @@ WORKDIR /usr/src/app
 COPY package.json package-lock.json ./
 RUN npm ci
 COPY . .
-RUN npm run build -- --prod
+RUN npm run build --
 
 ### STAGE 2: Run ###
 FROM nginx:latest

commit 086eaff9d710173bf13ce8155f43d8c10bb4c001

fix(build): build now uses production config for docker
regression introduced in #769

Dockerfile
@@ -8,7 +8,7 @@ WORKDIR /usr/src/app
 COPY package.json package-lock.json ./
 RUN npm ci
 COPY . .
-RUN npm run build --
+RUN npm run build -- --configuration=production
 
 ### STAGE 2: Run ###
 FROM nginx:latest