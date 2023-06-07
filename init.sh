4148c7ecb7b8814daef55dbfd3cadce845e9f3e9
Mike <michael.johanson@intel.com>
2023-06-07 08:14:10 -0700

    build(docker): set base image to nginx:mainline-alpine-slim (#1159)

--- init.sh
+++ init.sh
@@ -2,7 +2,5 @@ sed -i \
 -e "s|##RPS_SERVER##|$RPS_SERVER|g" \
 -e "s|##MPS_SERVER##|$MPS_SERVER|g" \
 -e "s|##VAULT_SERVER##|$VAULT_SERVER|g" \
--e "s|##MPSXAPIKEY##|$MPSXAPIKEY|g" \
--e "s|##RPSXAPIKEY##|$RPSXAPIKEY|g" \
 -e "s|##AUTH_MODE_ENABLED##|$AUTH_MODE_ENABLED|g" \
  /usr/share/nginx/html/*.js
