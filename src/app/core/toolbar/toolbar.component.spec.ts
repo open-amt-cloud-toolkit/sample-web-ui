2dbca39a29d3391ba94a7ea2ce08f4ba414cf803
rjbrache <rick.bracher@intel.com>
2023-08-04 14:55:15 -0700

    feat: display component versions (#1267)

--- src/app/core/toolbar/toolbar.component.spec.ts
+++ src/app/core/toolbar/toolbar.component.spec.ts
@@ -10,15 +10,24 @@ import { Router } from '@angular/router'
 import { AuthService } from 'src/app/auth.service'
 import { SharedModule } from 'src/app/shared/shared.module'
 import { ToolbarComponent } from './toolbar.component'
+import { of } from 'rxjs'
 
 describe('ToolbarComponent', () => {
   let component: ToolbarComponent
   let fixture: ComponentFixture<ToolbarComponent>
   let authService: { logout: any }
+  let mpsService: { getMPSVersion: any }
+  let getMPSVerSpy: jasmine.Spy
+  let rpsService: { getRPSVersion: any }
+  let getRPSVerSpy: jasmine.Spy
   // let logoutSpy: jasmine.Spy
   beforeEach(async () => {
     const routerSpy = jasmine.createSpyObj('Router', ['navigate'])
     authService = jasmine.createSpyObj('AuthService', ['logout'])
+    mpsService = jasmine.createSpyObj('AuthService', ['getMPSVersion'])
+    rpsService = jasmine.createSpyObj('AuthService', ['getRPSVersion'])
+    getMPSVerSpy = mpsService.getMPSVersion.and.returnValue(of({}))
+    getRPSVerSpy = rpsService.getRPSVersion.and.returnValue(of({}))
     const authServiceStub = {
       loggedInSubject: new EventEmitter<boolean>(false)
     }
@@ -28,7 +37,7 @@ describe('ToolbarComponent', () => {
       declarations: [ToolbarComponent],
       providers: [
         { provide: Router, useValue: routerSpy },
-        { provide: AuthService, useValue: { ...authServiceStub, ...authService } }
+        { provide: AuthService, useValue: { ...authServiceStub, ...authService, ...mpsService, ...rpsService } }
       ]
     })
       .compileComponents()
@@ -58,6 +67,16 @@ describe('ToolbarComponent', () => {
   it('should logout and redirect to login page', () => {
     component.logout()
     expect(authService.logout).toHaveBeenCalled()
+  })
+
+  it('should call getMPSVersion', () => {
+    expect(component).toBeTruthy()
+    expect(getMPSVerSpy).toHaveBeenCalled()
+  })
+
+  it('should call getRPSVersion', () => {
+    expect(component).toBeTruthy()
+    expect(getRPSVerSpy).toHaveBeenCalled()
   })
 
   it('should subscribe to loggedInSubject on init', () => {
--- src/app/core/toolbar/toolbar.component.ts
+++ src/app/core/toolbar/toolbar.component.ts
@@ -5,8 +5,13 @@
 
 import { Component, OnInit } from '@angular/core'
 import { MatDialog } from '@angular/material/dialog'
+import { MatSnackBar } from '@angular/material/snack-bar'
+import SnackbarDefaults from '../../shared/config/snackBarDefault'
+import { throwError } from 'rxjs'
+import { catchError, finalize } from 'rxjs/operators'
 import { AuthService } from 'src/app/auth.service'
 import { AboutComponent } from '../about/about.component'
+import { MpsVersion, RpsVersion } from 'src/models/models'
 
 @Component({
   selector: 'app-toolbar',
@@ -15,13 +20,41 @@ import { AboutComponent } from '../about/about.component'
 })
 export class ToolbarComponent implements OnInit {
   isLoggedIn = false
+  public isLoading = true
+  public rpsVersions?: RpsVersion
+  public mpsVersions?: MpsVersion
 
-  constructor (public dialog: MatDialog, public authService: AuthService) {
+  constructor (public snackBar: MatSnackBar, public dialog: MatDialog, public authService: AuthService) {
   }
 
   ngOnInit (): void {
+    this.isLoading = true
     this.authService.loggedInSubject.subscribe((value: any) => {
       this.isLoggedIn = value
+    })
+    this.authService.getMPSVersion().pipe(
+      catchError(err => {
+        // TODO: handle error better
+        this.snackBar.open($localize`Error retrieving MPS versions`, undefined, SnackbarDefaults.defaultError)
+        return throwError(err)
+      }),
+      finalize(() => {
+        this.isLoading = false
+      })
+    ).subscribe(data => {
+      this.mpsVersions = data
+    })
+    this.authService.getRPSVersion().pipe(
+      catchError(err => {
+        // TODO: handle error better
+        this.snackBar.open($localize`Error retrieving RPS versions`, undefined, SnackbarDefaults.defaultError)
+        return throwError(err)
+      }),
+      finalize(() => {
+        this.isLoading = false
+      })
+    ).subscribe(data => {
+      this.rpsVersions = data
     })
   }
 
