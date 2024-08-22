3e3f7a460612b15b47c8efe1e443690fab2cb7bc
Mike Johanson <michael.johanson@intel.com>
2024-08-22 09:37:59 -0700

    test: add coverage for device-cert-dialog
    Co-authored-by: Matt Primrose <matt-primrose@users.noreply.github.com>

--- src/app/devices/device-cert-dialog/device-cert-dialog.component.spec.ts
+++ src/app/devices/device-cert-dialog/device-cert-dialog.component.spec.ts
@@ -1,23 +1,60 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { DeviceCertDialogComponent } from './device-cert-dialog.component';
+import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog'
+import { MatSnackBar } from '@angular/material/snack-bar'
+import { of } from 'rxjs'
+import { DevicesService } from '../devices.service'
+import { DeviceCertDialogComponent } from './device-cert-dialog.component'
+import SnackbarDefaults from 'src/app/shared/config/snackBarDefault'
 
 describe('DeviceCertDialogComponent', () => {
-  let component: DeviceCertDialogComponent;
-  let fixture: ComponentFixture<DeviceCertDialogComponent>;
+  let component: DeviceCertDialogComponent
+  let fixture: ComponentFixture<DeviceCertDialogComponent>
+  let devicesServiceSpy: jasmine.SpyObj<DevicesService>
+  let snackBarSpy: jasmine.SpyObj<MatSnackBar>
+  let dialogRefSpy: jasmine.SpyObj<MatDialogRef<DeviceCertDialogComponent>>
 
   beforeEach(async () => {
+    const spyDevicesService = jasmine.createSpyObj('DevicesService', [
+      'pinDeviceCertificate',
+      'deleteDeviceCertificate'
+    ])
+    const spySnackBar = jasmine.createSpyObj('MatSnackBar', ['open'])
+    const spyDialogRef = jasmine.createSpyObj('MatDialogRef', ['close'])
+
     await TestBed.configureTestingModule({
-      imports: [DeviceCertDialogComponent]
-    })
-    .compileComponents();
+      imports: [DeviceCertDialogComponent],
+      providers: [
+        { provide: DevicesService, useValue: spyDevicesService },
+        { provide: MatSnackBar, useValue: spySnackBar },
+        { provide: MAT_DIALOG_DATA, useValue: { guid: 'test-guid', sha256Fingerprint: 'test-fingerprint' } },
+        { provide: MatDialogRef, useValue: spyDialogRef }
+      ]
+    }).compileComponents()
 
-    fixture = TestBed.createComponent(DeviceCertDialogComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(DeviceCertDialogComponent)
+    component = fixture.componentInstance
+    devicesServiceSpy = TestBed.inject(DevicesService) as jasmine.SpyObj<DevicesService>
+    snackBarSpy = TestBed.inject(MatSnackBar) as jasmine.SpyObj<MatSnackBar>
+    dialogRefSpy = TestBed.inject(MatDialogRef) as jasmine.SpyObj<MatDialogRef<DeviceCertDialogComponent>>
+  })
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+  it('should pin the device certificate and show a success message', () => {
+    devicesServiceSpy.pinDeviceCertificate.and.returnValue(of(null))
+
+    component.pin()
+
+    expect(devicesServiceSpy.pinDeviceCertificate).toHaveBeenCalledWith('test-guid', 'test-fingerprint')
+    expect(snackBarSpy.open).toHaveBeenCalledWith('Certificate pinned', 'Close', SnackbarDefaults.defaultSuccess)
+    expect(dialogRefSpy.close).toHaveBeenCalledWith(true)
+  })
+
+  it('should remove the device certificate and show a success message', () => {
+    devicesServiceSpy.deleteDeviceCertificate.and.returnValue(of(null))
+
+    component.remove()
+
+    expect(devicesServiceSpy.deleteDeviceCertificate).toHaveBeenCalledWith('test-guid')
+    expect(snackBarSpy.open).toHaveBeenCalledWith('Certificate removed', 'Close', SnackbarDefaults.defaultSuccess)
+    expect(dialogRefSpy.close).toHaveBeenCalledWith(false)
+  })
+})
