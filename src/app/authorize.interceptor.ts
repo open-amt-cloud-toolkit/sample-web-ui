9ded8cb32fbed97556acdf7c2a8057cc50892db2
Mike Johanson <michael.johanson@intel.com>
2024-09-25 10:51:45 -0700

    feat: indicate when device does not respond

--- src/app/authorize.interceptor.ts
+++ src/app/authorize.interceptor.ts
@@ -5,14 +5,16 @@
 
 import { inject, Injectable } from '@angular/core'
 import { HttpErrorResponse, HttpResponse, HttpInterceptorFn } from '@angular/common/http'
-import { throwError } from 'rxjs'
+import { of, throwError } from 'rxjs'
 import { AuthService } from './auth.service'
 import { catchError, tap } from 'rxjs/operators'
 import { MatDialog } from '@angular/material/dialog'
 import { DialogContentComponent } from './shared/dialog-content/dialog-content.component'
+import { MatSnackBar } from '@angular/material/snack-bar'
 export const AuthorizeInterceptor: HttpInterceptorFn = (request, next) => {
   const authService = inject(AuthService)
   const dialog = inject(MatDialog)
+  const snackbar = inject(MatSnackBar)
 
   if (request.url.toString().includes('/authorize') && !request.url.toString().includes('/authorize/redirection')) {
     // don't send auth token
@@ -45,6 +47,11 @@ export const AuthorizeInterceptor: HttpInterceptorFn = (request, next) => {
           dialog.open(DialogContentComponent, {
             data: { name: 'This item has been modified since it has been loaded. Please reload.' }
           })
+        } else if (error.status === 504) {
+          snackbar.open('Device did not respond. Double check connection settings and try again.', 'Dismiss', {
+            duration: 5000
+          })
+          return of()
         }
       }
       return throwError(() => {
