c392579c8d22542c6062fb3fa9ac0fc8d3d59e02
Mike <mjohanson@gesdate.com>
2025-01-29 09:26:44 -0700

    build: provide a dev enterprise configuration for better dx

---
+++ src/environments/environment.enterprise.dev.ts
@@ -0,0 +1,12 @@
+/*********************************************************************
+ * Copyright (c) Intel Corporation 2022
+ * SPDX-License-Identifier: Apache-2.0
+ **********************************************************************/
+
+export const environment = {
+  production: false,
+  cloud: false,
+  mpsServer: 'http://localhost:8181',
+  rpsServer: 'http://localhost:8181',
+  vault: ''
+}
